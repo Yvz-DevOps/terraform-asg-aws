46b8ef5e6d898c9b6743a368110710570fd3b791
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-21 00:33:04 -0400

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -9,7 +9,20 @@ resource "aws_security_group" "asg-sec-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "Allow HTTPS"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
